befe29167356ca38658bbdf08b56311130f0b6b8
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-10-16 23:16:46 +0300

    -create universal button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-// import {Header} from './components/Header';
-// import {Body} from './components/Body';
-// import {Footer} from './components/Footer';
-import {NewComponent} from './components/New component';
-
-const topCars = [
-    {manufacturer:'BMW', model:'m5cs'},
-    {manufacturer:'Mercedes', model:'e63s'},
-    {manufacturer:'Audi', model:'rs6'}
-]
+import './App.css'
+import {Button} from './components/Button';
 
 function App() {
-  return (
-    <div className="App">
-      {/*<Header title={'Header title'}/>*/}
-      {/*<Body titleForBody={'New Body'} />*/}
-      {/*<Footer/>*/}
-        <NewComponent cars={topCars}/>
-    </div>
-  );
+
+	const Button1Foo = (subscriber: string, age: number) => {
+		console.log(subscriber, age)
+	}
+
+	const Button2Foo = (subscriber: string) => {
+		console.log(subscriber)
+	}
+
+	const Button3Foo = () => {
+		console.log(' I am stupid button')
+	}
+
+	return (
+		<div className="App">
+			<Button name={'my youtube channel-1'} callBack={() => Button1Foo('I am Vasya', 21)}/>
+			<Button name={'my youtube channel-2'} callBack={() =>Button2Foo('I am Ivan')}/>
+			<Button name={'Stupid-button'} callBack={Button3Foo}/>
+		</div>
+	)
 }
 
 export default App;
